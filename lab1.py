e3a38451679cb891ba19c51695c026d1fabe3a76
Neel Kumar <neel1@cs.ualberta.ca>
2021-09-02 22:33:07 -0600

    Second push of lab1.py

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.get('https://www.google.com/')
+response = requests.get('https://raw.githubusercontent.com/wookieewrath/cmput404/master/lab1.py')
 print(response.content)
